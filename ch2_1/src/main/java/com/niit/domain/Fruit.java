663093d13ecc710b94f1c881b8546938c08cd831
q15751261559 <3328585084@qq.com>
2021-04-27 23:41:47 +0800

    4-26

--- ch2_1/src/main/java/com/niit/domain/Fruit.java
+++ ch2_1/src/main/java/com/niit/domain/Fruit.java
@@ -24,4 +24,28 @@ public class Fruit {
 
     public Fruit() {
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProducing_area() {
+        return producing_area;
+    }
+
+    public void setProducing_area(String producing_area) {
+        this.producing_area = producing_area;
+    }
 }
--- ch2_1/target/ch2_1/WEB-INF/jsp/fruit.jsp
+++ ch2_1/target/ch2_1/WEB-INF/jsp/fruit.jsp
@@ -6,7 +6,7 @@
   Time: 10:38
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
 <html>
 <head>
     <title>水果信息情况</title>
