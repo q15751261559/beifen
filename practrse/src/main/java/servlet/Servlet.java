063a157a7e281c6e60ca2e1d9bbbcdf2fad9a259
q15751261559 <3328585084@qq.com>
2021-06-07 01:01:18 +0800

    spring boot first

---
+++ practrse/src/main/java/servlet/Servlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "servlet", urlPatterns = "/lijunjie")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("https://www.baidu.com");
+    }
+}
---
+++ practrse/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 33285
+  Date: 2021/5/13
+  Time: 18:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+https://www.baidu.com/s?tn=02003390_46_hao_pg&wd=%E5%88%98%E5%BE%B7%E5%87%AF&ie=utf-8
+<form action="/lijunjie" method="post">
+    <input type="submit" value="刘德凯">
+</form>
+</body>
+</html>
